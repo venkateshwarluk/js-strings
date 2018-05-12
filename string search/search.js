6f028044e834f2801487c331ac63508d5e200041
venkatesh <ksrinuvenkatesh@gmail.com>
2018-05-12 16:46:17 +0530

    slice method added

---
+++ string search/search.js
@@ -0,0 +1,4 @@
+var str = "javascript is scripting language";
+var pos = str.search("scripting");
+var element = document.getElementById("demo")
+element.innerHTML = pos;
---
+++ string slice/slice.js
@@ -0,0 +1,4 @@
+var str = "javascript is scripting language";
+var pos = str.slice(10,18);
+var element = document.getElementById("demo")
+element.innerHTML = pos;
