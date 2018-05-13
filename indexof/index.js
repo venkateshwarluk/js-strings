d291e6a04ef5c85d87656bc1cc040b2598397907
venkatesh <ksrinuvenkatesh@gmail.com>
2018-05-13 15:21:53 +0530

    index method is added

--- indexof/index.js
+++ indexof/index.js
@@ -1,5 +1,5 @@
 'use strict'
 var str = "practise session of javascript index methods";
-var pos = str.indexOf("session");
+var pos = str.indexOf("session",3);
 var element = document.getElementById("demo")
 element.innerHTML = pos;
